2a68d4459933f90b842be05e0ef9ba8e12a132ca
JustinLung <justinlung77@gmail.com>
2022-05-09 20:10:43 +0200

    :white_check_mark: passed pwa audit!

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,21 +6,3 @@ workbox.routing.registerRoute(
   ({ request }) => request.destination === 'image',
   new workbox.strategies.CacheFirst()
 )
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/service-worker.js').then(
-      function (registration) {
-        // Registration was successful
-        console.log(
-          'ServiceWorker registration successful with scope: ',
-          registration.scope
-        )
-      },
-      function (err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err)
-      }
-    )
-  })
-}
