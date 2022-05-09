8a3cb8a71dfc7f872e3d6a2fcb671d2998e1313c
JustinLung <justinlung77@gmail.com>
2022-05-09 19:47:03 +0200

    :art: improved file structure

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,24 @@ importScripts(
 )
 
 workbox.routing.registerRoute(
-  ({request}) => request.destination === 'image',
+  ({ request }) => request.destination === 'image',
   new workbox.strategies.CacheFirst()
-);
+)
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/service-worker.js').then(
+      function (registration) {
+        // Registration was successful
+        console.log(
+          'ServiceWorker registration successful with scope: ',
+          registration.scope
+        )
+      },
+      function (err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err)
+      }
+    )
+  })
+}
