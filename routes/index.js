ece14c6f8e81ba97c54a16a221547816b1b0b363
JustinLung <justinlung77@gmail.com>
2022-05-18 09:41:09 +0200

    :zap: improved performance for project

--- routes/index.js
+++ routes/index.js
@@ -4,34 +4,44 @@ const {
   getAllFaq,
   getAllSections,
   getAllTips,
+  getTipById,
 } = require('../modules/api')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  Promise.all([
-    getAllPages(),
-    getAllSections(),
-    getAllTips(),
-    getAllFaq(),
-  ]).then((data) => {
-    const [pages, sections, tips, questions] = data
-    const groupBy = (items, key) =>
-      items.reduce(
-        (result, item) => ({
-          ...result,
-          [item[key]]: [...(result[item[key]] || []), item],
-        }),
-        {}
-      )
-    const categories = Object.entries(groupBy(questions, 'faq_category_id'))
-    res.render('index', {
-      sections,
-      questions,
-      pages,
-      categories,
-      tips,
+router
+
+  .get('/', (req, res) => {
+    Promise.all([
+      getAllPages(),
+      getAllSections(),
+      getAllTips(),
+      getAllFaq(),
+    ]).then((data) => {
+      const [pages, sections, tips, questions] = data
+      const groupBy = (items, key) =>
+        items.reduce(
+          (result, item) => ({
+            ...result,
+            [item[key]]: [...(result[item[key]] || []), item],
+          }),
+          {}
+        )
+      const categories = Object.entries(groupBy(questions, 'faq_category_id'))
+      res.render('index', {
+        sections,
+        questions,
+        pages,
+        categories,
+        tips,
+      })
     })
   })
-})
+
+  .get('/detail', (req, res) => {
+    const id = req.body.id
+    getTipById(id).then((data) => {
+      res.render('detail', { data: data.data[0] })
+    })
+  })
 
 module.exports = router
